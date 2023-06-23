0f0991fbc4abff35fc6d9461f68d54f37d615776
jing <jingli1524@gmail.com>
2023-06-23 15:32:01 -0400

    remove unneccesary mediaQuery

--- hooks/useMediaQuery.tsx
+++ hooks/useMediaQuery.tsx
@@ -16,22 +16,6 @@ export const useMediaQuery = ({ width, height }: { width: string; height: string
   return matches
 }
 
-export const useMediaQueryExact = ({ width, height }: { width: string; height: string }) => {
-  const [matches, setMatches] = useState(false)
-
-  useEffect(() => {
-    const media = window.matchMedia(`(width: ${width}) and (height: ${height})`)
-    if (media.matches !== matches) {
-      setMatches(media.matches)
-    }
-    const listener = () => setMatches(media.matches)
-    window.addEventListener('resize', listener)
-    return () => window.removeEventListener('resize', listener)
-  }, [matches, width, height])
-
-  return matches
-}
-
 export const useMediaQueryWidth = (query: any) => {
   const [matches, setMatches] = useState(false)
 
